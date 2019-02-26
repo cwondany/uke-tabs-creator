a49f1fb0867ea7c8d9f1948c75f316e5ac4682fa
cwondany <c.wondany@outlook.de>
2019-02-26 15:12:28 +0100

    example chord data is displayed

--- src/app/tab-creation/chord-list/chord-list.component.ts
+++ src/app/tab-creation/chord-list/chord-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chord } from '../chord.model';
 
 @Component({
   selector: 'app-chord-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chord-list.component.css']
 })
 export class ChordListComponent implements OnInit {
+
+  recipes: Chord[] = [
+    new Chord('1', '2', '3', '4'),
+    new Chord('1', '2', '3', '4'),
+  ];
 
   constructor() { }
 
