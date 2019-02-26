49d8a2fe74f5bba694b46390693adf43b9e81af6
cwondany <c.wondany@outlook.de>
2019-02-26 15:08:24 +0100

    Header added

---
+++ src/app/tab-creation/chord.model.ts
@@ -0,0 +1,13 @@
+export class Chord {
+  public gNote: string;
+  public cNote: string;
+  public aNote: string;
+  public eNote: string;
+
+  constructor(gNote, cNote, aNote, eNote) {
+    this.gNote = gNote;
+    this.cNote = cNote;
+    this.aNote = aNote;
+    this.eNote = eNote;
+  }
+}
